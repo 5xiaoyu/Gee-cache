05f3c0dd65ceeb823fa041f732dc13e7e380b146
5xiaoyu <xiaoyu_v5@126.com>
2021-01-20 21:06:39 +0800

    first commit

---
+++ lru/lru.go
@@ -0,0 +1,23 @@
+package lru
+
+import (
+	"container/list"
+)
+
+type Cache struct {
+	maxBytes 	int64
+	nBytes	 	int64
+	ll 			*list.List
+	cache 		map[string]*list.Element
+	//
+	OnEvicted	func(key string, value string)
+}
+
+type entry struct {
+	key string
+	value Value
+}
+
+type  Value interface {
+	Len() int
+}
